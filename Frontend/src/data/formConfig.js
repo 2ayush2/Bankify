613e01f26a2cde2c68f9a3d337c6cb2a1fd10371
Ayush Khadka <80869490+2ayush2@users.noreply.github.com>
2024-12-08 16:41:57 +0545

    Added data

--- Frontend/src/data/formConfig.js
+++ Frontend/src/data/formConfig.js
@@ -4,14 +4,16 @@ export const lcFormSteps = [
     title: "Application Information",
     description: "Upload the necessary documents to complete your application.",
     fields: [
-      { label: "Branch Name", type: "select", options: ["Branch 1", "Branch 2"], required: true,gridColumn: "col-span-12"  },
-      { label: "Date", type: "date", required: true,gridColumn: "col-span-12"  },
+      { label: "Branch Name", type: "select", options: ["Branch 1", "Branch 2"], required: true,gridColumn: "col-span-6"  },
+      { label: "Date", type: "date", required: true,gridColumn: "col-span-6"  },
       {
         label: "Mode of Transmission",
         type: "radio",
         options: ["Mail", "Swift/Telex", "Brief Cable details by mail"],
         required: true,
-        gridColumn: "col-span-12" 
+        gridColumn: "col-span-12" ,
+        tooltip: "Specify who will bear the charges.",
+
       },
     ],
   },
@@ -19,20 +21,28 @@ export const lcFormSteps = [
     title: "Letter of Credit (LC) Details",
     description: "Provide LC details like number and amount.",
     fields: [
-      { label: "Expiry Date of DC", type: "date", required: true,gridColumn: "col-span-6"  },
-      { label: "Expiry Location of DC", type: "text", required: true,gridColumn: "col-span-6"  },
+      { label: "Expiry Date of DC", type: "date", required: true,gridColumn: "col-span-5"  },
+      { label: "Expiry Location of DC", type: "text", required: true,gridColumn: "col-span-5"  },
+     
+      {
+        label: "Presentation Period",
+        type: "counter",
+        required: true
+        ,gridColumn: "col-span-2" 
+      },
       {
         label: "Charges",
         type: "toggle",
         options: ["Applicant’s Account", "Beneficiary’s Account"],
         required: true,
-        gridColumn: "col-span-6", 
+        gridColumn: "col-span-5", 
       },
       {
-        label: "Presentation Period",
-        type: "counter",
+        label: "Confirmation Instruction",
+        type: "radio",
+        options: ["Confirm", "May Add", "Without"],
         required: true
-        ,gridColumn: "col-span-6" 
+        ,gridColumn: "col-span-7" 
       },
     ],
   },
